70acafc2f03507528a11d5cfa5c22882b3dab73c
harshita <harshita1225@gmail.com>
2022-07-07 12:31:01 +0200

    task completed

---
+++ solution.js
@@ -0,0 +1,14 @@
+
+//task-1
+let myArray = ['Hey','how','are','you','doing','?'];
+console.log(myArray);
+
+//task-2
+let thirdElement = myArray[2];
+console.log(myArray[2]);
+console.log(thirdElement);
+
+//task-3
+myArray[3]='fourthElement';
+console.log(myArray);
+
